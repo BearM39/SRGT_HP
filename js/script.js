f4626da90e0626ba7650e7e7ef217b1eb6a7dc70
松隈涼太 <rnewbearm@gmail.com>
2019-05-31 00:19:50 +0900

    [update] Result画面新規作成

--- js/script.js
+++ js/script.js
@@ -2,34 +2,96 @@ var HOME_PATH = "./home.html";
       var RESULTS_PATH = "./results.html";
       var TEAM_DRIVER_PATH = "teamAndDriver.html";
 
-      function changeHtml(path){
+      function changeHtml(path, idName){
       $.ajax({
           url　: path,
           dataType : 'html',
           success　: function(data){
-              $('#content').html(data);
+              $(idName).html(data);
           },
           error: function(data){
-              $('#content').html(data);
+              $(idName).html(data);
           }
       });
     }
 
     $(window).on('load',function(){
-      changeHtml(HOME_PATH);
+      changeHtml(HOME_PATH, '#content');
       $(".carousel-control-next").click();
     });
 
     $(document).on('click', '#btnHome', function(){
-      changeHtml(HOME_PATH);
+      changeHtml(HOME_PATH, '#content');
     });
 
     $(document).on('click', '#btnResults', function(){
-      changeHtml(RESULTS_PATH);
+      changeHtml(RESULTS_PATH, '#content');
+      //init_results();
     });
 
     $(document).on('click', '#btnTeamAndDriver', function(){
-      changeHtml(TEAM_DRIVER_PATH);
+      changeHtml(TEAM_DRIVER_PATH, '#content');
+    });
+
+    $(document).on('click', '#teamRnk', function(){
+      var obj = document.getElementById('teamRnk');
+
+      if (obj.classList.contains("disable")){
+        obj.classList.remove('disable');
+      };
+
+      obj = document.getElementById('driverRnk');
+
+      if (!obj.classList.contains('disable')){
+        obj.classList.add('disable');
+      };
+      
+      $('#tblDriverRnk').css('display', 'none');
+      $('#tblTeamRnk').css('display', 'block');
+
+    });
+
+    // $(document).on('click', '#driverRnk', function(){
+    //   var obj = document.getElementById('driverRnk');
+
+    //   if (obj.classList.contains("disable")){
+    //     obj.classList.remove('disable');
+    //   };
+
+    //   obj = document.getElementById('teamRnk');
+
+    //   if (!obj.classList.contains('disable')){
+    //     obj.classList.add('disable');
+    //   };
+      
+    //   $('#tblTeamRnk').css('display', 'none');
+    //   $('#tblDriverRnk').css('display', 'block');
+
+    // });
+
+    $(document).on('click', '#driverRnk', function(){
+      init_results();
+    });
+
+    var init_results = function(){
+      var obj = document.getElementById('driverRnk');
+
+      if (obj.classList.contains("disable")){
+        obj.classList.remove('disable');
+      };
+
+      obj = document.getElementById('teamRnk');
+
+      if (!obj.classList.contains('disable')){
+        obj.classList.add('disable');
+      };
+      
+      $('#tblTeamRnk').css('display', 'none');
+      $('#tblDriverRnk').css('display', 'block');
+    };
+
+    $(document).on('click', '#rd1', function(){
+      changeHtml('./rd1Result.html', '#result-body');
     });
 
 
